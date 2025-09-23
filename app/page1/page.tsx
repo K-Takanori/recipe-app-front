1550cdae4891fe96e7b593740473d366eef91a78
K-Takanori <tabo.25.apr.1984@gmail.com>
2025-09-23 15:15:04 +0900

    first commit

---
+++ app/page1/page.tsx
@@ -0,0 +1,10 @@
+import Header from "@/components/Header";
+
+export default function Home() {
+  return (
+    <div>
+    <Header />
+    <p className="bg-red-300"> test</p>
+    </div>
+  )
+}
---
+++ components/Header.tsx
@@ -0,0 +1,9 @@
+export default function Header() {
+  return (
+    <header className="bg-blue-300 h-12 w-screen">
+      <div >
+        <a href="./" className=" text-black text-2xl">レシピ管理アプリ</a>
+      </div>
+    </header>
+  );
+}
